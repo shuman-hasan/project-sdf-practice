96b14f72de677a3d78c2a1ddb708c90c70b4efda
shuman-hasan <88977932+shuman-hasan@users.noreply.github.com>
2021-08-16 21:46:12 +0600

    html/css/bootstrap

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.slider-1').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:1200,
+      
+        
+      });
+})
